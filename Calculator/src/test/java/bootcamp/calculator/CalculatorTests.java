ff2ab83e66ec3fdf8a523a646382a2d432f54090
Rahul Rakshit <rahul.rakshit@posteo.net>
2020-04-02 20:06:37 +0200

    Do Calculator kata

--- Calculator/src/test/java/bootcamp/calculator/CalculatorTests.java
+++ Calculator/src/test/java/bootcamp/calculator/CalculatorTests.java
@@ -2,61 +2,50 @@ package bootcamp.calculator;
 
 import bootcamp.data.Params;
 import org.junit.Test;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 
 import java.math.BigDecimal;
 
 public class CalculatorTests {
-
     @Test
     public void testAdd() {
-        final Params params = new Params(new BigDecimal(3.2),
-                new BigDecimal(1.8),
-                "+");
+        final Params params = new Params(new BigDecimal(3.2), new BigDecimal(1.8), "+");
         final BigDecimal result = new Calculator().calculate(params);
-        assertEquals(new BigDecimal(5), result);
+        assertThat(result.doubleValue(), is(closeTo(5.0, 0.000000000000001)));
     }
 
     @Test
     public void testSubtract() {
-        final Params params = new Params(new BigDecimal(3.2),
-                new BigDecimal(0.2),
-                "-");
+        final Params params = new Params(new BigDecimal(3.2), new BigDecimal(0.2), "-");
         final BigDecimal result = new Calculator().calculate(params);
-        assertEquals(new BigDecimal(3), result);
+        assertThat(result.doubleValue(), is(closeTo(3.0, 0.000000000000001)));
     }
 
     @Test
     public void testMutiply() {
-        final Params params = new Params(new BigDecimal(2.25),
-                new BigDecimal(2),
-                "x");
+        final Params params = new Params(new BigDecimal(2.25), new BigDecimal(2), "x");
         final BigDecimal result = new Calculator().calculate(params);
-        assertEquals(new BigDecimal(4.5), result);
+        assertThat(result.doubleValue(), is(closeTo(4.5, 0.000000000000001)));
     }
 
     @Test
     public void testDivide() {
-        final Params params = new Params(new BigDecimal(3.2),
-                new BigDecimal(0.4),
-                "/");
+        final Params params = new Params(new BigDecimal(3.2), new BigDecimal(0.4), "/");
         final BigDecimal result = new Calculator().calculate(params);
-        assertEquals(new BigDecimal(8), result);
+        assertThat(result.doubleValue(), is(closeTo(8.0, 0.000000000000001)));
     }
 
     @Test(expected = ArithmeticException.class)
     public void testDivideBy0() {
-        final Params params = new Params(new BigDecimal(3.2),
-                new BigDecimal(0),
-                "/");
+        final Params params = new Params(new BigDecimal(3.2), new BigDecimal(0), "/");
         final BigDecimal result = new Calculator().calculate(params);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void testIncorrectOperator() {
-        final Params params = new Params(new BigDecimal(3.2),
-                new BigDecimal(1),
-                "*");
+        final Params params = new Params(new BigDecimal(3.2), new BigDecimal(1), "*");
         final BigDecimal result = new Calculator().calculate(params);
     }
 }
